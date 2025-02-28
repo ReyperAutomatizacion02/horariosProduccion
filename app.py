4913124ba21e0fa15c7089ed9fe41b49540dcc03
ReyperAutomatizacion02 <reyper.automatizacion.2@gmail.com>
2025-02-28 17:47:50 -0600

    first commit

--- app.py
+++ app.py
@@ -59,4 +59,4 @@ def run_script():
 # Ejecución de la aplicación Flask
 # ================================
 if __name__ == '__main__':                            
-    app.run(host='0.0.0.0', port=5000)    
+    app.run()    
