18e027cc0d64c05abb1d2c2e407a6044b33596a1
ReyperAutomatizacion02 <reyper.automatizacion.2@gmail.com>
2025-04-07 12:54:46 -0600

    Cambio de base de datos

--- app.py
+++ app.py
@@ -42,7 +42,7 @@ s = URLSafeTimedSerializer(app.config['SECRET_KEY']) # Inicializa el serializado
 
 # Configuración de la base de datos (SQLite en este caso)
 basedir = os.path.abspath(os.path.dirname(__file__)) # Obtiene el directorio base de la app
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://gerry:HQUeIYyrdII7mPAMfL9cRtWNvIVtaoXE@dpg-cv4tnkfnoe9s73dv8fb0-a.oregon-postgres.render.com/dbhorariosproduccion' # Usar la External Database URL de Render directamente
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://gerry:YIRUsyZ8le1QbXwPsRUcOQDV3lWqrerw@dpg-cvq1fk24d50c73c0tesg-a.oregon-postgres.render.com/dbautointelli' # Usar la External Database URL de Render directamente
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False # Desactiva el tracking de modificaciones de SQLAlchemy (opcional, pero recomendado para evitar warnings)
 
 db = SQLAlchemy(app) # Inicializa Flask-SQLAlchemy
